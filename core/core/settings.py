addcaaea700bb8c7f382e359ccf443a6d04558b6
DiegoBuencuero <diegobuencuero4095@gmail.com>
2025-11-19 01:02:20 -0300

    st

--- core/core/settings.py
+++ core/core/settings.py
@@ -128,10 +128,12 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'   # donde Django copiará todo
+
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
+    BASE_DIR / 'static',                 # donde están tus archivos originales
 ]
 
 # Default primary key field type
